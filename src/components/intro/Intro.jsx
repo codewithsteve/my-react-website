66b24d7d43477f337a3eac316fdbbf4873b9a66f
Stephen Dzakpasu <steve.dzakpasu@gmail.com>
2021-12-17 10:57:28 +0000

    finished home screen

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,25 @@
 import "./intro.scss";
 import Sidebar from "../sidebar/Sidebar";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      strings: ["Computer Science Student", "Aspiring Software Engineer"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <Sidebar />
+      <div className="container">
+        <h1>Hey there, &#128075; </h1>
+        <h1 className="name">I am Steve &#129299;</h1>
+        <h1 ref={textRef}></h1>
+      </div>
     </div>
   );
 }
